e39a7cbcc086b2fe4eb5974d1bc132e00b87a3fc
Kevin Nguyen <ngynkvn@gmail.com>
2020-03-22 16:59:44 -0400

    Link

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Banner(props) {
-    return <div>Teach. Leave. Learn.</div>
+    return <div>
+        <Link to="home">
+            <img width="100px" height="100px" src={"/tll_logo_no_bg.svg"} alt="Teach Leave Live" />
+        </Link>
+    </div>
 }
 
 export function WithBanner(Component) {
     return (p) => <div>
-        <Banner/>
+        <Banner />
         <Component {...p}></Component>
     </div>;
 }
