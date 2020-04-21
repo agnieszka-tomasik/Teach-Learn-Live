f80f4900110f22ed1674db6f31392547bcde2794
Alejandro Lopez <alejandrlopez@ufl.edu>
2020-04-21 02:02:26 -0400

    Implemented password restrictions with error message display, and local blocking on posts with error message display.

--- server/routes/forum/index.js
+++ server/routes/forum/index.js
@@ -109,8 +109,6 @@ router.route('/comment/delete').post((req, res) => {
         }
         else {
             const i = doc.comments.findIndex(c => c._id == comment._id);
-            //console.log(doc.comments);
-            //console.log(comment);
             doc.comments.splice(i, 1);
             ForumPost.findByIdAndUpdate(post._id, doc, (err, newDoc) => {
                 if (err) {
