5c216f21a82939519226588d63883162b07995a1
Kevin Nguyen <ngynkvn@gmail.com>
2020-04-21 05:46:33 -0400

    Show as clickable

--- client/src/views/Admin/AdminTable.js
+++ client/src/views/Admin/AdminTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./AdminTable.css";
 const AdminTable = ({ head, body, error }) => {
     return <div className="tableWrapper">
         <table className="table is-hoverable">
